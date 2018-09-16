af8b13ae439154fee98de3d76f14e3554b2d8830
semog <semog@users.noreply.github.com>
2018-09-15 23:40:08 -0700

    Add animated transition between holes.

--- lib/CurrentRoundPage.dart
+++ lib/CurrentRoundPage.dart
@@ -46,86 +46,98 @@ class _CurrentRoundPageState extends AmbientModeState<CurrentRoundPage> {
 
   void _nextHole() => setState(() {
         round.currentHoleIndex++;
+        _transitionAlignment = Alignment(0.0, 1.0);
       });
 
   void _previousHole() => setState(() {
         round.currentHoleIndex--;
+        _transitionAlignment = Alignment(0.0, -1.0);
       });
+
+  var _transitionAlignment = Alignment(0.0, 1.0);
 
   @override
   Widget build(BuildContext context) {
     if (!_loadData()) {
       return loadingPage;
     }
-    return GestureDetector(
-      key: Key(round.currentHole.hashCode.toString()),
-      onVerticalDragEnd: (details) {
-        if (details.primaryVelocity > 0) {
-          _previousHole();
-        } else if (details.primaryVelocity < 0) {
-          _nextHole();
-        }
-      },
-      child: Scaffold(
-        backgroundColor: Colors.black,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(top: 10.0),
-                child: Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-                  Column(
+    return AnimatedSwitcher(
+      duration: const Duration(milliseconds: 200),
+      transitionBuilder: (child, animation) => ScaleTransition(
+            child: child,
+            scale: animation,
+            alignment: _transitionAlignment,
+          ),
+      child: GestureDetector(
+        key: Key(round.currentHole.hashCode.toString()),
+        onVerticalDragEnd: (details) {
+          if (details.primaryVelocity > 0) {
+            _previousHole();
+          } else if (details.primaryVelocity < 0) {
+            _nextHole();
+          }
+        },
+        child: Scaffold(
+          backgroundColor: Colors.black,
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Text(
+                          fmtHoleNum(round.currentHoleNum),
+                          style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold, color: Colors.blueGrey[200]),
+                        ),
+                        Text(
+                          'Hole',
+                          style: TextStyle(fontSize: 18.0, color: Colors.blueGrey[300]),
+                        ),
+                      ],
+                    ),
+                    Icon(
+                      Icons.golf_course,
+                      color: Colors.blue[200],
+                      size: 35.0,
+                    ),
+                  ]),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: _strokeCountPadding),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
-                      Text(
-                        fmtHoleNum(round.currentHoleNum),
-                        style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold, color: Colors.blueGrey[200]),
+                      IconButton(
+                        icon: Icon(Icons.remove_circle_outline),
+                        iconSize: 50.0,
+                        color: _hideableColor(_buttonColor),
+                        onPressed: _decrementStrokes,
                       ),
                       Text(
-                        'Hole',
-                        style: TextStyle(fontSize: 18.0, color: Colors.blueGrey[300]),
+                        '${round.currentStrokeCount}',
+                        style: TextStyle(fontSize: _strokeFontSize, fontWeight: FontWeight.bold, color: Colors.white),
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.add_circle_outline),
+                        iconSize: 50.0,
+                        color: _hideableColor(_buttonColor),
+                        onPressed: _incrementStrokes,
                       ),
                     ],
                   ),
-                  Icon(
-                    Icons.golf_course,
-                    color: Colors.blue[200],
-                    size: 35.0,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: _cumulativePadding),
+                  child: Text(
+                    'Score: ${round.currentScore}',
+                    style: TextStyle(fontSize: 17.0, color: Colors.blueGrey[300]),
                   ),
-                ]),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: _strokeCountPadding),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    IconButton(
-                      icon: Icon(Icons.remove_circle_outline),
-                      iconSize: 50.0,
-                      color: _hideableColor(_buttonColor),
-                      onPressed: _decrementStrokes,
-                    ),
-                    Text(
-                      '${round.currentStrokeCount}',
-                      style: TextStyle(fontSize: _strokeFontSize, fontWeight: FontWeight.bold, color: Colors.white),
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.add_circle_outline),
-                      iconSize: 50.0,
-                      color: _hideableColor(_buttonColor),
-                      onPressed: _incrementStrokes,
-                    ),
-                  ],
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: _cumulativePadding),
-                child: Text(
-                  'Score: ${round.currentScore}',
-                  style: TextStyle(fontSize: 17.0, color: Colors.blueGrey[300]),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
